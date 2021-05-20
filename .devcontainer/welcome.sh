3d2f8a1dafc938b1d83389e2f9732b8fca7da4ce
KEINOS <github@keinos.com>
2021-05-20 20:18:59 +0900

    feat: stable dev container

--- .devcontainer/welcome.sh
+++ .devcontainer/welcome.sh
@@ -3,11 +3,22 @@
 # Remove go-build* files in temp
 rm -rf /tmp/go-build* 2>/dev/null 1>/dev/null
 
+# Shoe welcome message
+echo "========================================================================="
+echo " Welcome To QiiTask Development Container"
+echo "========================================================================="
 echo "- OS: $(head -n1 </etc/issue)"
 echo "- $(go version)"
 echo "- Current path: $(pwd)"
 echo "- Current user: $(whoami)"
 echo "- Time: $(date)"
-echo "- To run linters                : ./.github/run-tests-lint.sh"
-echo "- To run unit tests and coverage: ./.github/run-tests-coverage.sh"
-echo "- To run full tests before merge: ./.github/run-tests-merge.sh"
+echo "* Useful command and scripts:"
+echo "  - To run unit and coverage tests : $ run-coverage"
+echo "  - To run lint and static analysis: $ run-lint"
+echo "  - To run full tests before merge : $ run-all"
+echo "  - To update README.md documents  : $ update-docs"
+echo "  - To build binary                : $ build-app --help"
+echo "  - To display this message again  : $ welcome"
+echo "  - To search a string in a file   : $ rg foobar ."
+echo "  - To run the app before build    : $ go run ./main list"
+echo "========================================================================="
