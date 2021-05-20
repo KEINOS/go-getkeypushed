e1accddf967d7ebe65c3afabfe90d268413ff7ad
KEINOS <github@keinos.com>
2021-05-20 22:49:18 +0900

    Update checkTTY.go

--- src/checkTTY.go
+++ src/checkTTY.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/mattn/go-tty"
 )
 
 func checkTTY(returnChan chan<- string, t *tty.TTY) {
-	time.Sleep(10 * time.Second)
-
 	r, err := t.ReadRune()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open TTY in checkTTY().\nErrMsg: %v\n", err)
